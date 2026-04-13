ab428e0db6b1c15ae35a7311b35b48a9f1cd77f8
Ayaad72 <ayaadalavi@gmail.com>
2026-04-13 23:26:38 +0500

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function ParallelSimulation() {
     }
   }, []);
 
-  // Save reseeeesdfffffffddsdffsddddeeeeeeeeeults to localStorage whenever they change
+  // Save reseeeesdffffeeeeeeeults to localStorage whenever they change
   useEffect(() => {
     localStorage.setItem("simulationResults", JSON.stringify(results));
   }, [results]);
