ed1d579b26b3059e34c1ed18c23f789661e6a4e5
Ayaad72 <ayaadalavi@gmail.com>
2026-04-16 13:58:30 +0500

    pushing the latest changes

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function ParallelSimulation() {
     }
   }, []);
 
-  // Save results sssssto localStorage whenever they change
+  // Save results ssssffffsdfsdfssto localStorage whenever they change
   useEffect(() => {
     localStorage.setItem("simulationResults", JSON.stringify(results));
   }, [results]);
