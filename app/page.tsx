6cb3be4b5253f2a4c851a7dd51e5a4eac531ea4f
Ayaad72 <ayaadalavi@gmail.com>
2026-04-17 16:22:39 +0500

    all changes

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function ParallelSimulation() {
     setResults([newResult, ...results]);
   };
 
-  // Clear all history
+  // Clesssssssssssssar all history
   const clearHistory = () => {
     if (
       confirm(
